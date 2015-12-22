c56c94f1317aa587bc588d88821cbdf6052b40c3
David Yeung <quxbaz@gmail.com>
2015-12-22 15:37:01 -0500

    Initial commit

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+var _ = require('./lib/underscore/underscore');
+require('./lib/chai/chai').should();
+// var $ = require('./lib/jquery/dist/jquery');
+var timer = require('../timer');
+
+describe('timer.js', function() {
+
+  before(function() {
+  });
+
+  it("", function() {
+  });
+
+});
