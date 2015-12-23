379465f39bea23a40de9f540367d852dd5c66947
David Yeung <quxbaz@gmail.com>
2015-12-23 13:32:15 -0500

    Test suite

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,16 @@
 var _ = require('./lib/underscore/underscore');
 require('./lib/chai/chai').should();
-// var $ = require('./lib/jquery/dist/jquery');
 var timer = require('../timer');
 
 describe('timer.js', function() {
 
   var t;
   var fn = function() {};       // No-op function.
+
+  // Tests if @val is approximately equal to @compareTo within +/- @deviation.
+  function approx(val, compareTo, deviation) {
+    return (val >= compareTo - deviation) && (val <= compareTo + deviation);
+  }
 
   beforeEach(function() {
     t && t.stop();
@@ -17,6 +21,14 @@ describe('timer.js', function() {
     new timer.Timer();
   });
 
+  it("starts and stops a timer.", function(done) {
+    t.start();
+    t.on('tick', function(timerData) {
+      t.stop();
+      done();
+    });
+  });
+
   it("expects a valid event.", function() {
     t.on.bind(t, 'tick', fn).should.not.throw();
   });
@@ -25,32 +37,75 @@ describe('timer.js', function() {
     t.on.bind(t, 'foofoofoo', fn).should.throw();
   });
 
-  it("configures a tick interval.", function() {
-
-  });
-
-  it.only("listens to a timer for 10 ticks.", function(done) {
-    t = new timer.Timer({tickInterval: 10})
+  it("configures a tick interval.", function(done) {
+    t = new timer.Timer({tickInterval: 100});
+    var startTime = performance.now();
     t.on('tick', function(timerData) {
-      if (timerData.tick >= 10) {
-        // <TODO> Bug here: postMessage('stop') can take longer than
-        // 10ms to fire and cause this block to run multiple times.
+      if (timerData.tick == 2) {
+        (approx(performance.now() - startTime, 200, 50)).should.be.true;
         t.stop();
         done();
       }
     }).start();
   });
 
-  it("starts a timer.", function(done) {
-  });
-
-  it("stops a timer.", function(done) {
-    // t.stop('tick')
-    done();
+  it("listens to a timer for 10 ticks.", function(done) {
+    t = new timer.Timer({tickInterval: 10})
+    t.on('tick', function(timerData) {
+      if (timerData.tick >= 10) {
+        t.stop();
+        done();
+      }
+    }).start();
   });
 
   it("measures a timer for 10 ticks", function(done) {
-    done();
+    t = new timer.Timer({tickInterval: 10});
+    var startTime = performance.now();
+    t.on('tick', function(timerData) {
+      if (timerData.tick == 10) {
+        (approx(performance.now() - startTime, 100, 50)).should.be.true;
+        t.stop();
+        done();
+      }
+    }).start();
+  });
+
+  it("measures a timer for 100 ticks", function(done) {
+    t = new timer.Timer({tickInterval: 5});
+    var startTime = performance.now();
+    t.on('tick', function(timerData) {
+      if (timerData.tick == 100) {
+        (approx(performance.now() - startTime, 500, 75)).should.be.true;
+        t.stop();
+        done();
+      }
+    }).start();
+  });
+
+  it("measures a minimal tick against setTimeout.", function(done) {
+    var start = performance.now();
+    var tickCount = 100;
+    var tickInterval = 1;
+    var i = 0;
+    (function fn() {
+      if (i < tickCount)
+        setTimeout(fn, tickInterval);
+      else {
+        var timeoutTicksPerSec = (performance.now() - start) / tickCount;
+        t = new timer.Timer({tickInterval: tickInterval});
+        t.on('tick', function(timerData) {
+          if (timerData.tick == tickCount) {
+            var timerTicksPerSec = (performance.now() - timerData.start) / tickCount;
+            approx(timerTicksPerSec, timeoutTicksPerSec, 0.5).should.be.true;
+            console.log('timeout TPS:', timeoutTicksPerSec);
+            console.log('timer TPS:', timerTicksPerSec);
+            done();
+          }
+        }).start();
+      }
+      i++;
+    })();
   });
 
 });
